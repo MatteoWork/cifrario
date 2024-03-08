1f26daad498ff2923bee2c4fb67c27d9bcf6b9e7
Matteo <matteoamodio17@gmail.com>
2024-03-08 15:08:30 +0100

    Fixato il cifrario contrario

--- cifrarioContrario.js
+++ cifrarioContrario.js
@@ -2,6 +2,18 @@ function decifrarioDiCesare(cryptedPhrase, shift) {
     return cifrarioDiCesare(cryptedPhrase, 26 - shift);
 }
 
+function cifrarioDiCesare(phrase, shift) {
+    return phrase.toUpperCase().split('').map(char => {
+            if (char >= 'A' && char <= 'Z') {
+                const nuovoCharCode = ((char.charCodeAt(0) - 65 + shift) % 26) + 65;
+                return String.fromCharCode(nuovoCharCode);
+            } else {
+                return char;
+            }
+        })
+        .join('');
+}
+
 // Esempio di utilizzo
 const cryptedPhrase = "KHOOR, ZRUOG!";
 const shift = 3;
